8feb1a45bbba76f5fee4431fc453a398264d8dce
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2023-12-21 15:14:38 +0100

    easy code

---
+++ main.js
@@ -0,0 +1,7 @@
+const nome = prompt('dammi il tuo nome')
+
+const cognome = prompt('dammi il tuo cognome')
+
+const colorepref = prompt('dammi il tuo colore preferito')
+
+document.writeln(nome, cognome, colorepref)
